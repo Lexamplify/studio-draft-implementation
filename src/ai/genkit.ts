974175faa1eb28a14dbde16532942213f355d520
Lexamplify <lexamplify@gmail.com>
2025-07-19 15:23:34 +0530

    upgraded the model from 1.5 --> 2.0 && improved translation prompt

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,12 +1,12 @@
-/* import {genkit} from 'genkit';
+import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
   plugins: [googleAI()],
   model: 'googleai/gemini-2.0-flash',
 });
- */
 
+/* 
 import {genkit} from 'genkit';
 import {googleAI, gemini15Flash} from '@genkit-ai/googleai';
 
@@ -14,3 +14,4 @@ export const ai = genkit({
   plugins: [googleAI()],
   model: gemini15Flash, // Using the proper Gemini 1.5 Flash model
 });
+ */
